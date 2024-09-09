2bd1705f2ebabd165baaafe74b22bcc8c53153dd
johnson2427 <blakeejohnson39@gmail.com>
2024-09-09 16:21:10 -0500

    feat: retrieving spot price

--- example.py
+++ example.py
@@ -1,13 +1,21 @@
-from ape import project, accounts, networks
+import requests
+
+
+def get_eth_spot_price():
+    url = 'https://api.coingecko.com/api/v3/simple/price'
+    params = {
+        'ids': 'ethereum',
+        'vs_currencies': 'usd'
+    }
+    response = requests.get(url, params=params)
+    eth_price = response.json()['ethereum']['usd']
+    return eth_price
 
 
 def main():
-    deployer = accounts.load('ape_test')
-    price_feed_address = "0x5f4ec3df9cbd43714fe2740f5e3616155c5b8419"
-    with networks.parse_network_choice("ethereum:sepolia"):
-        eth_price_consumer = deployer.deploy(project.spot_price, price_feed_address)
-        print(f"Contract deployed at: {eth_price_consumer.address}")
+    eth_price = get_eth_spot_price()
+    print(f"The current spot price of ETH is {eth_price}")
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
