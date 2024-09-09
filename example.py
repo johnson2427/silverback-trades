1b65c5043d2ca8414f8d933c2d49e04fd9d34423
johnson2427 <blakeejohnson39@gmail.com>
2024-09-09 13:16:24 -0500

    feat: move to sepolia for testing

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ from ape import project, accounts, networks
 def main():
     deployer = accounts.load('ape_test')
     price_feed_address = "0x5f4ec3df9cbd43714fe2740f5e3616155c5b8419"
-    with networks.parse_network_choice("ethereum:mainnet"):
+    with networks.parse_network_choice("ethereum:sepolia"):
         eth_price_consumer = deployer.deploy(project.spot_price, price_feed_address)
         print(f"Contract deployed at: {eth_price_consumer.address}")
 
